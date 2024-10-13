6c9205c890191e71a0a3d9f29fad2c659549463d
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-10-13 20:29:30 +0530

    Made Customer Login page

---
+++ src/main/java/controller/CustomerLoginFormController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class CustomerLoginFormController {
+
+    @FXML
+    private JFXTextField txtCustomerEmail;
+
+    @FXML
+    private JFXPasswordField txtCustomerPassword;
+
+    @FXML
+    void btnCustomerSignInOnAction(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/controller/DashFormController.java
+++ src/main/java/controller/DashFormController.java
@@ -2,6 +2,12 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashFormController {
 
@@ -12,7 +18,13 @@ public class DashFormController {
 
     @FXML
     void btnCustomerLoginOnAction(ActionEvent event) {
-
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_login.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "ERROR : " + e);
+        }
     }
 
 }
