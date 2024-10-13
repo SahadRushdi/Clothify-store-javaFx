feac349cb880df89e088b5581f22d5565bc12a83
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-10-13 11:59:16 +0530

    first commit

---
+++ src/main/java/controller/DashFormController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashFormController {
+
+    @FXML
+    void btnAdminLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomerLoginOnAction(ActionEvent event) {
+
+    }
+
+}
