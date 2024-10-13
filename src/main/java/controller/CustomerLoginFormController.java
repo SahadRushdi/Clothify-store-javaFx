f327ed3d1a7674ae4c2e0b10bb37ff9dcaa71308
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-10-13 21:12:24 +0530

    Made Customer Dress Section Page

--- src/main/java/controller/CustomerLoginFormController.java
+++ src/main/java/controller/CustomerLoginFormController.java
@@ -4,6 +4,12 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomerLoginFormController {
 
@@ -15,7 +21,13 @@ public class CustomerLoginFormController {
 
     @FXML
     void btnCustomerSignInOnAction(ActionEvent event) {
-
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dress_selection.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "ERROR : " + e);
+        }
     }
 
 }
---
+++ src/main/java/controller/DressSelectionFormController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DressSelectionFormController {
+
+    @FXML
+    void btnGentsDressOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnKidsDressOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLadiesDressOnAction(ActionEvent event) {
+
+    }
+
+}
