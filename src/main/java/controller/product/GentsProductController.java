ee28e0fcc68ae1e891b927cb167b328cd296707a
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-10-16 20:57:54 +0530

    Added DB connecton required maven dependency

--- src/main/java/controller/product/GentsProductController.java
+++ src/main/java/controller/product/GentsProductController.java
@@ -1,4 +1,36 @@
 package controller.product;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class GentsProductController {
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colSize;
+
+    @FXML
+    private TableColumn<?, ?> colSupplier;
+
+    @FXML
+    private TableView<?> tblGentsProducts;
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
 }
